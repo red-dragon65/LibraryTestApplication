08bf2a500a5f68f25853286423c88a5a471cc793
mushy <mujtabaashfaq@yahoo.com>
2024-09-30 18:15:14 -0600

    Did some type cleaning

--- SuperCalc/src/main/java/com/example/supercalc/Strategies/Subtraction.kt
+++ SuperCalc/src/main/java/com/example/supercalc/Strategies/Subtraction.kt
@@ -3,7 +3,7 @@ package com.example.supercalc.Strategies
 class Subtraction : CalcStrat {
     override fun gigaCalc(number: List<Int>): Int {
 
-        var result = 0;
+        var result = 0
 
         for(i in number){
 
